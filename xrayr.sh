62b954f4497dbbbe7590aac4e9c917be01c35c34
SuperWonz <101009246+SuperWonz@users.noreply.github.com>
2022-04-13 10:27:54 +0700

    Update xrayr.sh

--- xrayr.sh
+++ xrayr.sh
@@ -369,7 +369,7 @@ Install_xrayr() {
 # Initialization step
 clear
 while true; do
-  echo "-----XrayR của Tài Copy -----"
+  echo "-----XrayR của Wonz -----"
   echo "Địa chỉ dự án và tài liệu trợ giúp: Chưa nghĩ ra  "
   echo "Vui lòng nhập một số để Thực Hiện Câu Lệnh:"
   for ((i = 1; i <= ${#operation[@]}; i++)); do
