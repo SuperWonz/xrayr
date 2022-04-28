d51f17359347c12e261f46bcd98bd305a81effbf
SuperWonz <101009246+SuperWonz@users.noreply.github.com>
2022-04-28 18:37:33 +0700

    Update xrayr.sh

--- xrayr.sh
+++ xrayr.sh
@@ -1,190 +1,91 @@
-#!/usr/bin/env bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
+#!/binstall
 
-# Current folder
-cur_dir=$(pwd)
-# Color
-red='\033[0;31m'
-green='\033[0;32m'
-#yellow='\033[0;33m'
-plain='\033[0m'
-operation=(Install stop start Update UpdateConfig logs restart delete)
-# Make sure only root can run our script
-[[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] Chưa vào root kìa !, vui lòng xin phép ROOT trước!" && exit 1
-
-#Check system
-check_sys() {
-  local checkType=$1
-  local value=$2
-  local release=''
-  local systemPackage=''
-
-  if [[ -f /etc/redhat-release ]]; then
-    release="centos"
-    systemPackage="yum"
-  elif grep -Eqi "debian|raspbian" /etc/issue; then
-    release="debian"
-    systemPackage="apt"
-  elif grep -Eqi "ubuntu" /etc/issue; then
-    release="ubuntu"
-    systemPackage="apt"
-  elif grep -Eqi "centos|red hat|redhat" /etc/issue; then
-    release="centos"
-    systemPackage="yum"
-  elif grep -Eqi "debian|raspbian" /proc/version; then
-    release="debian"
-    systemPackage="apt"
-  elif grep -Eqi "ubuntu" /proc/version; then
-    release="ubuntu"
-    systemPackage="apt"
-  elif grep -Eqi "centos|red hat|redhat" /proc/version; then
-    release="centos"
-    systemPackage="yum"
-  fi
-
-  if [[ "${checkType}" == "sysRelease" ]]; then
-    if [ "${value}" == "${release}" ]; then
-      return 0
-    else
-      return 1
-    fi
-  elif [[ "${checkType}" == "packageManager" ]]; then
-    if [ "${value}" == "${systemPackage}" ]; then
-      return 0
-    else
-      return 1
-    fi
-  fi
+# bảng điều khiển
+red() {
+	echo -e "\033[31m\033[01m$1\033[0m"
 }
 
-# Get version
-getversion() {
-  if [[ -s /etc/redhat-release ]]; then
-    grep -oE "[0-9.]+" /etc/redhat-release
-  else
-    grep -oE "[0-9.]+" /etc/issue
-  fi
+green() {
+	echo -e "\033[32m\033[01m$1\033[0m"
 }
 
-# CentOS version
-centosversion() {
-  if check_sys sysRelease centos; then
-    local code=$1
-    local version="$(getversion)"
-    local main_ver=${version%%.*}
-    if [ "$main_ver" == "$code" ]; then
-      return 0
-    else
-      return 1
-    fi
-  else
-    return 1
-  fi
+yellow() {
+	echo -e "\033[33m\033[01m$1\033[0m"
 }
 
-get_char() {
-  SAVEDSTTY=$(stty -g)
-  stty -echo
-  stty cbreak
-  dd if=/dev/tty bs=1 count=1 2>/dev/null
-  stty -raw
-  stty echo
-  stty $SAVEDSTTY
+# Xác định hệ thống và xác định các phụ thuộc cài đặt hệ thống
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Alpine")
+PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "apk add -f")
+PACKAGE_REMOVE=("apt -y remove" "apt -y remove" "yum -y remove" "yum -y remove")
+
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
+
+for i in "${CMD[@]}"; do
+	SYS="$i" && [[ -n $SYS ]] && break
+done
+
+for ((int = 0; int < ${#REGEX[@]}; int++)); do
+	[[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
+done
+
+[[ -z $SYSTEM ]] && red "VPS không tương thích, vui lòng sử dụng hệ điều hành chính" && exit 1
+
+archAffix() {
+	case "$(uname -m)" in
+  x86_64 | x64 | amd64) return 0 ;;
+	aarch64 | arm64) return 0 ;;
+	*) red "Cầu hình CPU không được hỗ trợ, tập lệnh sắp thoát!" ;;
+	esac
+
+	return 0
 }
-error_detect_depends() {
-  local command=$1
-  local depend=$(echo "${command}" | awk '{print $4}')
-  echo -e "[${green}Info${plain}] Bắt đầu cài đặt các gói ${depend}"
-  ${command} >/dev/null 2>&1
-  if [ $? -ne 0 ]; then
-    echo -e "[${red}Error${plain}] Cài đặt gói không thành công ${red}${depend}${plain}"
-    exit 1
-  fi
+
+install() {
+	install_XrayR
+	clear
+	makeConfig
 }
 
-# Pre-installation settings
-pre_install_docker_compose() {
-
- echo -e "[1] 4gdatasieure.xyz"
-  echo -e "[2] 4gdatasieure.xyz"
-  read -p "Web đang sử dụng:" api_host
-  if [ "$api_host" == "1" ]; then
-    api_host="https://4gdatasieure.xyz"
-  elif [ "$api_host" == "2" ]; then
-    api_host="https://4gdatasieure.xyz"
-  else 
-    api_host="https://4gdatasieure.xyz"
-  fi
-
-  echo "--------------------------------"
-  echo "Bạn đã chọn ${api_host}"
-  echo "--------------------------------"
-  
-  read -p " ID nút (Node_ID):" node_id
-  [ -z "${node_id}" ] && node_id=0
-  echo "-------------------------------"
-  echo -e "Node_ID: ${node_id}"
-  echo "-------------------------------"
-  
-
-  #giới hạn thiết bị
-read -p "Giới hạn thiết bị :" DeviceLimit
-  [ -z "${DeviceLimit}" ] && DeviceLimit="0"
-  echo "-------------------------------"
-  echo "Thiết bị tối đa là: ${DeviceLimit}"
-  echo "-------------------------------"
-  
-  #CertDomain:
-read -p "ip hoăc domain:" CertDomain
- [ -z "${CertDomain}" ] && CertDomain="0"
-  echo "-------------------------------"
-  echo "CertDomain: ${CertDomain}"
-  echo "-------------------------------"
-  
-  
+install_XrayR() {
+	[[ -z $(type -P curl) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} curl
+	[[ -z $(type -P socat) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} socat
+	bash <(curl -Ls https://raw.githubusercontent.com/AikoCute/XrayR-release/master/install.sh)
 }
- 
 
+makeConfig() {
+    echo "---------------"
 
-# Config docker
-config_docker() {
-  cd ${cur_dir} || exit
-  echo "Bắt đầu cài đặt các gói"
-  install_dependencies
-  echo "Tải tệp cấu hình DOCKER"
-  cat >docker-compose.yml <<EOF
-version: '3'
-services: 
-  xrayr: 
-    image: crackair/xrayr:latest
-    volumes:
-      - ./config.yml:/etc/XrayR/config.yml # thư mục cấu hình bản đồ
-      - ./dns.json:/etc/XrayR/dns.json 
-    restart: always
-    network_mode: host
-EOF
-  cat >dns.json <<EOF
-{
-    "servers": [
-        "1.1.1.1",
-        "8.8.8.8",
-        "localhost"
-    ],
-    "tag": "dns_inbound"
-}
-EOF
-  cat >config.yml <<EOF
+	read -p "Số node ID :" makeNodeID
+	echo "---------------"
+	read -p "Giới hạn số thiết bị, nếu không muốn giới hạn hãy nhập 0 :" makeLimitdevice
+	echo "---------------"
+	read -p "Giới hạn tốc độ, nếu không muốn giới hạn nhập 0 :" makeSpeedlimit
+	echo "---------------"
+	read -p "CertMode:" CertMode
+	echo "---------------"
+        read -p "CertDomain:" CertDomain
+	echo "---------------"
+
+	rm -f /etc/XrayR/config.yml
+	if [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]]; then
+		curl https://get.acme.sh | sh -s email=script@github.com
+		source ~/.bashrc
+		bash ~/.acme.sh/acme.sh --upgrade --auto-upgrade
+	fi
+         cat <<EOF >/etc/XrayR/config.yml
 Log:
   Level: none # Log level: none, error, warning, info, debug 
   AccessPath: # /etc/XrayR/access.Log
   ErrorPath: # /etc/XrayR/error.log
-DnsConfigPath: # /etc/XrayR/dns.json Path to dns config, check https://xtls.github.io/config/base/dns/ for help
-RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/base/route/ for help
-OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/base/outbound/ for help
+DnsConfigPath: # /etc/XrayR/dns.json # Path to dns config, check https://xtls.github.io/config/dns.html for help
+RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/routing.html for help
+InboundConfigPath: # /etc/XrayR/custom_inbound.json # Path to custom inbound config, check https://xtls.github.io/config/inbound.html for help
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/outbound.html for help
 ConnetionConfig:
   Handshake: 4 # Handshake time limit, Second
-  ConnIdle: 10 # Connection idle time limit, Second
+  ConnIdle: 30 # Connection idle time limit, Second
   UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
   DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
   BufferSize: 64 # The internal cache size of each connection, kB 
@@ -194,13 +95,13 @@ Nodes:
     ApiConfig:
       ApiHost: "https://4gdatasieure.xyz"
       ApiKey: "superwonzvjpprono1"
-      NodeID: 41
+      NodeID: $makeNodeID
       NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
       Timeout: 30 # Timeout for the api request
       EnableVless: false # Enable Vless for V2ray Type
       EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
+      SpeedLimit: $makeSpeedlimit # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: $makeLimitdevice # Local settings will replace remote settings, 0 means disable
       RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file
     ControllerConfig:
       ListenIP: 0.0.0.0 # IP address you want to listen
@@ -221,9 +122,9 @@ Nodes:
           Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
           ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
       CertConfig:
-        CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "node1.test.com" # Domain to cert
-        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
+        CertMode: "$CertMode" # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "$CertDomain" # Domain to cert
+        CertFile: /etc/XrayR/cert/node1.test.com.cert  # Provided if the CertMode is file
         KeyFile: /etc/XrayR/cert/node1.test.com.key
         Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
         Email: test@me.com
@@ -231,168 +132,9 @@ Nodes:
           ALICLOUD_ACCESS_KEY: aaa
           ALICLOUD_SECRET_KEY: bbb
 EOF
-  sed -i "s|ApiHost:.*|ApiHost: \"${api_host}\"|" ./config.yml
-  sed -i "s|NodeID:.*|NodeID: ${node_id}|" ./config.yml
-  sed -i "s|DeviceLimit:.*|DeviceLimit: ${DeviceLimit}|" ./config.yml
- # sed -i "s|CertDomain:.*|CertDomain: \"${CertDomain}\"|" ./config.yml
-  }
-
-# Install docker and docker compose
-install_docker() {
-  echo -e "bắt đầu cài đặt DOCKER "
- sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-systemctl start docker
-systemctl enable docker
-  echo -e "bắt đầu cài đặt Docker Compose "
-curl -fsSL https://get.docker.com | bash -s docker
-curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-  echo "khởi động Docker "
-  service docker start
-  echo "khởi động Docker-Compose "
-  docker-compose up -d
-  echo
-  echo -e "Đã hoàn tất cài đặt phụ trợ ！"
-  echo -e "0 0 */3 * *  cd /root/${cur_dir} && /usr/local/bin/docker-compose pull && /usr/local/bin/docker-compose up -d" >>/etc/crontab
-  echo -e "Cài đặt cập nhật thời gian kết thúc đã hoàn tất! hệ thống sẽ update sau [${green}24H${plain}] Từ lúc bạn cài đặt"
+	xrayr start
+	green "Đã cài đặt và cập nhật XrayR với bảng điều khiển thành công！"
+	exit 1
 }
 
-install_check() {
-  if check_sys packageManager yum || check_sys packageManager apt; then
-    if centosversion 5; then
-      return 1
-    fi
-    return 0
-  else
-    return 1
-  fi
-}
-
-install_dependencies() {
-  if check_sys packageManager yum; then
-    echo -e "[${green}Info${plain}] Kiểm tra kho EPEL ..."
-    if [ ! -f /etc/yum.repos.d/epel.repo ]; then
-      yum install -y epel-release >/dev/null 2>&1
-    fi
-    [ ! -f /etc/yum.repos.d/epel.repo ] && echo -e "[${red}Error${plain}] Không cài đặt được kho EPEL, vui lòng kiểm tra." && exit 1
-    [ ! "$(command -v yum-config-manager)" ] && yum install -y yum-utils >/dev/null 2>&1
-    [ x"$(yum-config-manager epel | grep -w enabled | awk '{print $3}')" != x"True" ] && yum-config-manager --enable epel >/dev/null 2>&1
-    echo -e "[${green}Info${plain}] Kiểm tra xem kho lưu trữ EPEL đã hoàn tất chưa ..."
-
-    yum_depends=(
-      curl
-    )
-    for depend in ${yum_depends[@]}; do
-      error_detect_depends "yum -y install ${depend}"
-    done
-  elif check_sys packageManager apt; then
-    apt_depends=(
-      curl
-    )
-    apt-get -y update
-    for depend in ${apt_depends[@]}; do
-      error_detect_depends "apt-get -y install ${depend}"
-    done
-  fi
-  echo -e "[${green}Info${plain}] Đặt múi giờ thành Hồ Chí Minh GTM+7"
-  ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh  /etc/localtime
-  date -s "$(curl -sI g.cn | grep Date | cut -d' ' -f3-6)Z"
-
-}
-
-#update_image
-Update_xrayr() {
-  cd ${cur_dir}
-  echo "Tải hình ảnh DOCKER"
-  docker-compose pull
-  echo "Bắt đầu chạy dịch vụ DOCKER"
-  docker-compose up -d
-}
-
-#show last 100 line log
-
-logs_xrayr() {
-  echo "100 dòng nhật ký chạy sẽ được hiển thị"
-  docker-compose logs --tail 100
-}
-
-# Update config
-UpdateConfig_xrayr() {
-  cd ${cur_dir}
-  echo "đóng dịch vụ hiện tại"
-  docker-compose down
-  pre_install_docker_compose
-  config_docker
-  echo "Bắt đầu chạy dịch vụ DOKCER"
-  docker-compose up -d
-}
-
-restart_xrayr() {
-  cd ${cur_dir}
-  docker-compose down
-  docker-compose up -d
-  echo "Khởi động lại thành công!"
-}
-delete_xrayr() {
-  cd ${cur_dir}
-  docker-compose down
-  cd ~
-  rm -Rf ${cur_dir}
-  echo "đã xóa thành công!"
-}
-#Stop
-stop_xrayr() {
-  cd ${cur_dir}
-  docker-compose down
-  echo "Đã dừng!"
-}
-  start_xrayr() {
-  cd ${cur_dir}
-  docker-compose up -d
-  echo "Đã Chạy!"
-  }
-
-# Install xrayr
-Install_xrayr() {
-  pre_install_docker_compose
-  config_docker
-  install_docker
-}
-
-# Initialization step
-clear
-while true; do
-  echo "-----XrayR của Tài copy -----"
-  echo "Địa chỉ dự án và tài liệu trợ giúp:  "
-  echo "Vui lòng nhập một số để Thực Hiện Câu Lệnh:"
-  for ((i = 1; i <= ${#operation[@]}; i++)); do
-    hint="${operation[$i - 1]}"
-    echo -e "${green}${i}${plain}) ${hint}"
-  done
-  read -p "Vui lòng chọn một số và nhấn Enter (Enter theo mặc định ${operation[0]}):" selected
-  [ -z "${selected}" ] && selected="1"
-  case "${selected}" in
-  1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 )
-    echo
-    echo "Bắt Đầu : ${operation[${selected} - 1]}"
-    echo
-    ${operation[${selected} - 1]}_xrayr
-    break
-    ;;
-  *)
-    echo -e "[${red}Error${plain}] Vui lòng nhập số chính xác [1-8]"
-    ;;
-  esac
-done
+install
